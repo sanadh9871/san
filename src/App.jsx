e8579c6ee14a3cc5514e6938396c6e935735216e
Sandesh <sanadh9871@gmail.com>
2024-12-09 21:38:17 -0600

    new commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-// const App =() => {
-//   console.log('Hello from component')
+const App = () => {
+    return (
+        <div>
+            <h1>Greetings</h1>
+
+            <Hello name='George' />
+            <Hello name='Daisy' />
+        </div>
+    )
+}
+const Hello = (props) => {
+    console.log(props)
+    return (
+        <div>
+
+            <p>Hello {props.name}</p>
+        </div>
+    )
+}
+export default App
+//
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log('Hello',now, a+b)
+//
 //   return (
 //     <div>
-//       <p>Hello World</p>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
 //     </div>
 //   )
 // }
 // export default App
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log('Hello',now, a+b)
-
-  return (
-    <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-    </div>
-  )
-}
-export default App
