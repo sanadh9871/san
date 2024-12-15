69671f4bf94d899dca8e11b431775b7ff89fca6d
Sandesh <sanadh9871@gmail.com>
2024-12-15 14:51:19 -0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,74 @@
-const App = () => {
-    return (
-        <div>
-            <h1>Greetings</h1>
-
-            <Hello name='George' />
-            <Hello name='Daisy' />
-        </div>
-    )
-}
-const Hello = (props) => {
-    console.log(props)
-    return (
-        <div>
-
-            <p>Hello {props.name}</p>
-        </div>
-    )
-}
-export default App
-//
 // const App = () => {
-//   const now = new Date()
-//   const a = 10
-//   const b = 20
-//   console.log('Hello',now, a+b)
-//
+//   console.log('san')
+//   const friends = [
+//     { name: 'Peter', age: 4 },
+//     { name: 'Maya', age: 10 },
+//   ]
+
 //   return (
+    
 //     <div>
-//       <p>Hello world, it is {now.toString()}</p>
-//       <p>
-//         {a} plus {b} is {a + b}
-//       </p>
+//       <p>{friends[0].name} {friends[0].age}</p>
+//       <p>{friends[1].name} {friends[1].age}</p>
 //     </div>
 //   )
 // }
+
 // export default App
+
+
+const Header = ({ courseName, numParts }) => {
+  return (
+    <div>
+      <h1>{courseName}</h1>
+      <p>Number of parts: {numParts}</p>
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>{name} {exercises} exercises</p>
+  );
+};
+
+const Total = ({ totalExercises }) => {
+  return (
+    <p>Total number of exercises is {totalExercises}</p>
+  );
+};
+
+const App = () => {
+  console.log('San')
+  const course = {
+            name: 'Half Stack application development',
+            parts: [
+              {
+                name: 'Fundamentals of React',
+                exercises: 10
+              },
+              {
+                name: 'Using props to pass data',
+                exercises: 7
+              },
+              {
+                name: 'State of a component',
+                exercises: 14
+              }
+            ]
+          };
+          const totalExercises = course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
+
+
+          return (
+            <div>
+              <Header courseName={course.name} numParts={course.parts.length} />
+              {course.parts.map(part => (
+                <Part name={part.name} exercises={part.exercises} key={part.name} />
+              ))}
+              <Total totalExercises={totalExercises} />
+            </div>
+          );
+        };
+        export default App
+        
